6a79a6dadaaf4eb5f986a5e25f5997ebdd9fa90a
Magesh.B <Mageshmca2001@gmail.com>
2024-10-16 11:21:59 +0530

    first commit

---
+++ config/Database.js
@@ -0,0 +1,9 @@
+import {Sequelize} from "sequelize";
+
+const db = new Sequelize ('sql12737958','sql12737958','vccZGhPDZy',{
+    host:"sql12.freesqldatabase.com",
+    dialect:"mysql"
+
+});
+
+export default db;
